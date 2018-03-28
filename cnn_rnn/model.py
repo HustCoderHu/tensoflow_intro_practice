47ebb22648251b6d7a056635b2c3d379ed0d4451
HustCoderHu <coderhzx@gmail.com>
2018-03-28 20:37:13 +0800

    CNN和RNN 分开

--- cnn_rnn/model.py
+++ cnn_rnn/model.py
@@ -65,9 +65,8 @@ class Simplest:
 
     # RNN
     # n_step = 50
-    n_hidden = 8
-    
-    
+    n_hidden = 6
+
     # if out.shape[0] < n_step:
     #   sequence_length = (out.shape[0])
     
@@ -90,6 +89,100 @@ class Simplest:
     pr_shape(states) # (n_hidden,)
 
     return final_state
+
+class CNN:
+  def __init__(self, num_classes, data_format='NHWC', dtype=tf.float32):
+    self.training = tf.placeholder(tf.bool, [])
+    
+    self.w_initer=tc.layers.xavier_initializer(tf.float32)
+    self.data_format = data_format
+    self.dtype = dtype
+    self.layer_data_format = 'channels_last' if data_format == 'NHWC' \
+        else 'channels_first'
+    
+    # self.conv3d = tl.Conv3D(filters=64, kernel_size=(1,7,7), strides=(1,2,2),
+    #     padding=self.layer_data_format, use_bias=False, kernel_initializer=self.w_initer)
+
+    axis = 1 if self.data_format=="NCHW" else -1
+    self.bn = tl.BatchNormalization(axis=axis, scale=False, fused=True)
+    # out = self.bn(in, training=True) False for eval
+
+    return
+  
+  def Conv2D(self, filters, kernel_size, strides=1, padding='valid', use_bias=False):
+    return tl.Conv2D(filters, kernel_size, strides,
+        padding=padding, data_format=self.layer_data_format, use_bias=use_bias, 
+        kernel_initializer=self.w_initer)
+  def FC(self, units, use_bias=False):
+    return tl.Dense(units, use_bias=use_bias, kernel_initializer=self.w_initer)
+  def Pool2D(self, pool_size, strides, pool_type):
+    if pool_type == 'max':
+      return tl.MaxPooling2D(pool_size, strides, data_format=self.layer_data_format)
+    else :
+      return tl.AveragePooling2D(pool_size, strides, data_format=self.layer_data_format)
+  
+  def global_avg_pool(self, x):
+    if self.data_format == 'NHWC':
+      out = tf.reduce_mean(x, (1, 2))
+    else :
+      out = tf.reduce_mean(x, (2, 3))
+    return out
+  
+  def __call__(self, inputs):
+    # self.training
+    pr_shape = lambda var : print(var.shape)
+    
+    # CNN
+    out = self.Conv2D(32, 5)(inputs)
+    out = tf.nn.relu(out)
+    out = self.Pool2D(2, 2, 'max')(out)
+
+    out = self.Conv2D(64, 5)(out)
+    out = tf.nn.relu(out)
+    out = self.Pool2D(2, 2, 'max')(out)
+    # out = self.bn(out, training=self.training)
+
+    out = tl.Flatten()(out)
+    out = self.FC(128)(out)
+    out = tl.Dropout(0.5)(out, training=self.training)
+    out = self.FC(32)(out)
+    pr_shape(out)
+    
+    return out
+
+class RNN:
+  def __init__(self, rnntype, n_hidden, n_classes):
+    self.rnntype = rnntype
+    self.n_hidden = n_hidden
+    self.n_classes = n_classes
+
+  def __call__(self, inputs):
+    pr_shape = lambda var : print(var.shape)
+
+    if rnntype == "GRU" :
+      print("rnntype: " + rnntype)
+      cell = rnn.GRUCell(n_hidden)
+    else :
+      print("rnntype: " + rnntype)
+      cell = rnn.LSTMCell(n_hidden)
+    
+    initial_state = cell.zero_state(inputs.shape[0], tf.float32)
+
+    # dynamic_rnn inputs shape = [batch_size, max_time, ...]
+    # outs shape = [batch_size, max_time, cell.output_size]
+    # states shape = [batch_size, cell.state_size]
+    n_step = int(inputs.shape[1]) # n_step
+
+    outs, states = nn.dynamic_rnn(cell, out, sequence_length=(n_step, ),
+      initial_state=initial_state, dtype=tf.float32)
+    final_state = states[-1] # list len = n_steps
+    pr_shape(states) # (n_hidden,)
+
+    FC = tl.Dense(self.n_classes, use_bias=True, 
+        kernel_initializer=tc.layers.xavier_initializer(tf.float32))
+    outs = FC(final_state)
+
+    return outs
 
 if __name__ == '__main__':
   model = Simplest(8)
---
+++ cnn_rnn/monsess_train_eval.py
@@ -0,0 +1,175 @@
+import time
+import os
+import os.path as path
+import tensorflow as tf
+
+import dataset
+import simple_model as smodel
+import runhooks
+
+
+def main():
+  train_dir = r'F:\Lab408\jinzhengu\root\shuffled_divided\train'
+  eval_dir = r'F:\Lab408\jinzhengu\root\shuffled_divided\eval'
+  log_dir = r'F:\Lab408\jinzhengu\root\monitored_sess_log'
+  ckpt_dir = path.join(log_dir, 'ckpts')
+  # model_dir = r'F:\Lab408\jinzhengu\root\monitored_sess_log'
+  
+  eval_interval = 20
+  
+  save_summary_steps = 5
+  save_ckpts_steps = 100
+  train_batchsz = 20
+  eval_batchsz = 50
+  # eval_steps = 40
+  epoch = 900
+  img_num = 870 * 2
+  max_steps = (img_num * epoch) // train_batchsz
+
+
+  # ------------------------------ prepare input ------------------------------
+  dset = dataset.MyDataset(train_dir, eval_dir)
+  prefetch_batch = None
+  iter_dict = {
+    'train': dset.train(train_batchsz, prefetch_batch),
+    'eval': dset.eval(eval_batchsz, prefetch_batch)
+  }
+  # train_iter = dset.train(train_batchsz, prefetch_batch)
+  # eval_iter = dset.eval(eval_batchsz, prefetch_batch)
+  # dict_tsr_handle = {
+    # 'train': train_iter.string_handle(),
+    # 'eval': eval_iter.string_handle()
+  # }
+
+  holder_handle = tf.placeholder(tf.string, [])
+  iter = tf.data.Iterator.from_string_handle(
+      holder_handle, iter_dict['train'].output_types)
+  # next_elem = iter.get_next()
+  inputx, labels, filename = iter.get_next()
+  inputx = tf.reshape(inputx, [-1, 200, 250, 3])
+  inputx = tf.transpose(inputx, [0, 3, 1, 2]) # nchw
+  # eval_x.set_shape([eval_batchsz, 200, 250, 3])
+
+  # train_x, train_y, train_fname = dset.train(train_batchsz, prefetch_batch)
+  # train_x.set_shape([train_batchsz, 200, 250, 3])
+  # eval_x, eval_y, eval_fname = dset.eval(eval_batchsz, prefetch_batch)
+  # eval_x.set_shape([eval_batchsz, 200, 250, 3])
+
+  # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ build graph \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  model = smodel.Simplest('NCHW')
+  # model = smodel.Simplest('NHWC')
+  logits = model(inputx)
+  with tf.name_scope('cross_entropy'):
+    loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+    
+  with tf.name_scope('accuracy'):
+    _pred = tf.argmax(logits, axis=1, output_type=tf.int32)
+    acc_vec = tf.equal(labels, _pred)
+    acc = tf.reduce_mean(tf.cast(acc_vec, tf.float32))
+    
+  with tf.name_scope('optimizer'):
+    optimizer = tf.train.AdamOptimizer(1e-4)
+    train_op = optimizer.minimize(loss, tf.train.get_or_create_global_step())
+  
+
+  # ||||||||||||||||||||||||||||||  hooks ||||||||||||||||||||||||||||||
+  # >>>  logging
+  tf.logging.set_verbosity(tf.logging.INFO)
+  # global_step = tf.train.get_or_create_global_step()
+  # tf.identity(global_step, 'g_step')
+  # tf.identity(loss, 'cross_entropy')
+  # tf.identity(acc, 'accuracy')
+  # tensor_lr = optimizer._lr_t
+
+  tensors = {
+    'step': tf.train.get_or_create_global_step().name,
+    'loss': loss.name,
+    'accuracy': acc.name
+  }
+  logging_hook = tf.train.LoggingTensorHook(
+    tensors= tensors,
+    every_n_iter=10
+  )
+
+  # >>>  summary
+  summary_conf = {
+    'dir': log_dir,
+    'saved_steps': save_summary_steps
+  }
+  summary_protobuf = {
+    'loss': tf.summary.scalar('cross_entropy', loss),
+    'accuracy': tf.summary.scalar('accuracy', acc)
+  }
+  # print(list(summary_protobuf.values()))
+  # summary_loss = tf.summary.scalar('cross_entropy', loss)
+  # summary_acc = tf.summary.scalar('accuracy', acc)
+  # summary_lr = tf.summary.scalar('lr', optimizer._lr_t)
+  # global_step = 
+  # merged_op = tf.summary.merge_all()
+  # summary_hook = tf.train.SummarySaverHook(
+  #   save_steps=1,
+  #   output_dir= ckpt_dir,
+  #   summary_op= merged_op
+  # )
+  
+  # >>> main run hook
+  eval_hook = runhooks.RunHook(
+    iter_dict= iter_dict,
+    eval_steps= eval_interval,
+    train_op= train_op,
+    training= model.training,
+    holder_handle= holder_handle,
+    summary_conf= summary_conf,
+    summary_protobuf= summary_protobuf,
+  )
+
+  # >>>  checkpoit saver
+  ckpt_saver_hook = runhooks.CkptSaverHook(
+    ckpt_dir,
+    save_steps= save_ckpts_steps
+  )
+  # ckpt_saver_hook = tf.train.CheckpointSaverHook(
+  #   checkpoint_dir= ckpt_dir,
+  #   save_steps= save_ckpts_steps,
+  # )
+
+  all_hooks = [
+      # logging_hook,
+      # summary_hook,
+      eval_hook,
+      ckpt_saver_hook,
+      # tf.train.StopAtStepHook(max_steps),
+      # tf.train.NanTensorHook(loss)
+  ]
+
+  # ////////////////////////////// session config //////////////////////////////
+  sess_conf = tf.ConfigProto()
+  sess_conf.gpu_options.allow_growth = True
+  sess_conf.gpu_options.per_process_gpu_memory_fraction = 0.75
+  
+  sess_creator = tf.train.ChiefSessionCreator(
+      # scaffold=scaffold,
+      # master='',
+      config=sess_conf,
+      checkpoint_dir=ckpt_dir
+  )
+  # print('end')
+  # return
+
+  # ------------------------------  start  ------------------------------
+  with tf.train.MonitoredSession(
+    session_creator= sess_creator,
+    hooks= all_hooks,
+    stop_grace_period_secs= 3600
+    ) as mon_sess:
+    while not mon_sess.should_stop():
+      step = mon_sess.run(tf.train.get_global_step()) # arg from retval of _EvalHook before_run()
+      # training, step = mon_sess.run([model.training, tf.train.get_global_step()]) # arg from retval of _EvalHook before_run()
+      # if not training:
+        # print('step {}: eval xxxxxxxxx'.format(step))
+      # print(lr)
+  return
+
+if __name__ == '__main__':
+  main()
+
