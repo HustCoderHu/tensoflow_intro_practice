c59722e9b05b1835f2b817301dc18105a3d916fd
HustCoderHu <coderhzx@gmail.com>
2018-05-29 20:12:11 +0800

    videoRoot 位置调整

--- cnn_rnn/var_config.py
+++ cnn_rnn/var_config.py
@@ -3,6 +3,8 @@ from os.path import join as pj
 
 cwd = r'/home/hzx/fireDetect-hzx/log20180517'
 cwd = r'/home/hzx/fireDetect-hzx/log20180524'
+videoRoot = pj(cwd, 'all_data')
+cwd = r'/home/hzx/fireDetect-hzx/log20180529'
 
 # cwd = r'D:\Lab408\cnn_rnn\20180517'
 # cwd = r'D:\Lab408\cnn_rnn\20180524'
@@ -10,7 +12,7 @@ cwd = r'/home/hzx/fireDetect-hzx/log20180524'
 log_dir = pj(cwd, 'train_eval_log')
 ckpt_dir = path.join(log_dir, 'ckpts')
 
-videoRoot = pj(cwd, 'all_data')
+# videoRoot = pj(cwd, 'all_data')
 labeljson = r'/home/hzx/all_data/label.json'
 
 evalSet = [47, 48, 49, 50, 27, 33, 21, 32]
